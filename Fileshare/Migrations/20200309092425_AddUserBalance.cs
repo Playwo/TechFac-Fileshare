fdd8b106f68c94c0f9e4f939970de289ddfedbde
Elias Wolf <eliaswolf2001@t-online.de>
2020-03-09 10:34:46 +0100

    Added UserBalance

---
+++ Fileshare/Migrations/20200309092425_AddUserBalance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fileshare.Migrations
+{
+    public partial class AddUserBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "Balance",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Users");
+        }
+    }
+}
