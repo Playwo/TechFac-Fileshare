e8d8bbd127c8ac69bc9145ce2d1e9d7b2c87f9f8
Playwo <eliaswolf2001@t-online.de>
2020-03-16 23:05:10 +0100

    Added PreviewOptions Migration

---
+++ Fileshare/Migrations/20200316220457_AddPreviewOptions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Fileshare.Migrations
+{
+    public partial class AddPreviewOptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PreviewOptions",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(nullable: false),
+                    Redirection = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PreviewOptions", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_PreviewOptions_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PreviewOptions");
+        }
+    }
+}
